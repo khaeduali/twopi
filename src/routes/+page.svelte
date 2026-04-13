1f047a9535b64d436310bfd91fe111f9005cd76e
khaeduali <alikha06@edu.umea.se>
2026-04-13 14:48:27 +0200

    layout med js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,27 @@
 <script>
+	import { onMount } from "svelte";
+
+const circle_count = 8;
+
+onMount(() => init());
+
+function init() {
+    let circles = document.getElementsByClassName("small-circle")
+
+    for (let i = 0; i < circle_count; i++) {
+        circles[i].style.offsetDistance = "calc(100% * " + (i / circle_count) + ")";
+    }
+}
 
 </script>
 
 <main>
     <div id="main-circle">
+        {#each {length: 8}}
         <div class="small-circle">
 
-        </div>
-        <div class="small-circle">
-
-        </div>
-        <div class="small-circle">
-
-        </div>
-        <div class="small-circle">
-
-        </div>
-        <div class="small-circle">
-
-        </div>
-        <div class="small-circle">
-
-        </div>
-        <div class="small-circle">
-
-        </div>
-        <div class="small-circle">
-
-        </div>
+        </div>  
+        {/each}
     </div>
 </main>
 
@@ -50,8 +44,6 @@ main {
     border: 16px inset var(--main-color);
     border-radius: 50%;
 
-    box-shadow: 0px 0px 20px 0px var(--main-color);
-
     transition: all 0.5s;
 }
 
@@ -64,13 +56,21 @@ main {
     border-radius: 50%;
 
     grid-area: 1/1;
-    offset: circle(320px) calc(100%*sibling-index()/sibling-count()) 0deg;
+    offset: circle(36vh) 0deg;
 
     transition: inherit;
 }
 
 .small-circle:hover {
     border-width: 5px;
+}
+
+.small-circle:nth-child(4), .small-circle:nth-child(8) {
+    border-style: inset;
+}
+
+.small-circle:nth-child(5), .small-circle:nth-child(6), .small-circle:nth-child(7) {
+    border-color: var(--inner-color);
 }
 
 </style>
