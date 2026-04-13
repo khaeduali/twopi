d121da8b24840d4d1504299e21a5999e5f58c4a5
khaeduali <alikha06@edu.umea.se>
2026-04-13 09:47:25 +0200

    circle wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,29 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+let {
+    children
+} = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+    <link rel="icon" href={favicon} />
+    </svelte:head>
 
-{@render children()}
+    {@render children()}
+
+<style>
+:global(body),
+:global(html) {
+    margin: 0%;
+    padding: 0%;
+    height: 100vh;
+    width: 100vw;
+
+	--bg-color: black;
+	--inner-color: #FFEF99AA;
+	--inner-hover-color: #FFC4FFAA;
+	--main-color: #FFEF99;
+	--main-hover-color: #FFC4FF;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,69 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+</script>
+
+<main>
+    <div id="main-circle">
+        <div class="small-circle">
+
+        </div>
+        <div class="small-circle">
+
+        </div>
+        <div class="small-circle">
+
+        </div>
+        <div class="small-circle">
+
+        </div>
+        <div class="small-circle">
+
+        </div>
+    </div>
+</main>
+
+<style>
+main {
+    position: fixed;
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    background-color: var(--bg-color);
+}
+
+#main-circle {
+    display: grid;
+
+    width: 50vh;
+    height: 50vh;
+    margin: auto;
+    
+    border: 16px inset var(--main-color);
+    border-radius: 50%;
+
+    box-shadow: 0px 0px 20px 0px var(--main-color);
+
+    transition: all 0.5s;
+}
+
+#main-circle:hover {
+    border: 25px inset var(--main-hover-color);
+    box-shadow: 0px 0px 30px 0px var(--main-hover-color);
+}
+
+.small-circle {
+    width: 100px;
+    height: 100px;
+    border: 16px solid;
+    border-color: inherit;
+    background-color: var(--bg-color);
+    border-radius: 50%;
+
+    transition: inherit;
+}
+
+.small-circle:hover {
+    border-width: 5px;
+}
+
+</style>
